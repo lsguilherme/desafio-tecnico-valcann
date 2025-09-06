77cf74908925029fed4a46c44466d76745c1795f
Guilherme Lacerda <83317386+lsguilherme@users.noreply.github.com>
2025-09-06 20:33:03 -0300

    chore: create project

---
+++ api-usuarios/src/test/java/com/example/desafio_valcann/DesafioValcannApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafio_valcann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioValcannApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
