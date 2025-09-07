247f2abeadc6b1557c022f6f7424324e15eba5a3
Guilherme Lacerda <83317386+lsguilherme@users.noreply.github.com>
2025-09-07 17:58:33 -0300

    feat: install Swagger and documentation UserController

--- api-usuarios/src/main/java/com/example/desafio_valcann/controllers/UserController.java
+++ api-usuarios/src/main/java/com/example/desafio_valcann/controllers/UserController.java
@@ -3,11 +3,21 @@ package com.example.desafio_valcann.controllers;
 import com.example.desafio_valcann.dtos.UserDto;
 import com.example.desafio_valcann.dtos.UserPage;
 import com.example.desafio_valcann.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
+@Tag(name = "Users", description = "API REST simples para listagem de usuários.\n\n" +
+        "Funcionalidades:\n\n" +
+        "- Listar usuários de forma paginada com filtros a partir de um .json.\n\n" +
+        "- Buscar um usuário por ID.")
 public class UserController {
     private final UserService userService;
 
@@ -15,6 +25,41 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(summary = "Listar usuários paginados",
+            description = "Busca usuários pelo nome ou email com filtros opcionais",
+            parameters = {
+                @Parameter(
+                        name = "page",
+                        description = "Número da página."
+                ),
+                @Parameter(
+                        name = "pageSize",
+                        description = "Quantidade de itens por página.\n\n" +
+                        "- Máximo de itens por página = 50."
+                ),
+                @Parameter(
+                        name = "q",
+                        description = "Filtro por nome ou email do usuário",
+                        example = "Gabriela"
+                ),
+                @Parameter(
+                        name = "role",
+                        description = "Filtro por função do usuário",
+                        schema = @Schema(
+                                type = "string",
+                                allowableValues = {"manager", "analyst", "admin", "viewer"},
+                                description = "Função no sistema"
+                        )
+                ),
+            @Parameter(
+                    name = "is_active",
+                    description = "Filtro por status ativo do usuário."
+            )
+            })
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Lista de usuários retornada com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping
     public ResponseEntity<UserPage> findAllUsers(
             @RequestParam(defaultValue = "1") int page,
@@ -27,6 +72,21 @@ public class UserController {
         return ResponseEntity.ok(response);
     }
 
+    @Operation(summary = "Buscar usuário por ID", 
+            description = "Retorna um usuário específico pelo seu ID",
+            parameters = {
+            @Parameter(
+                    name = "id",
+                    description = "ID do usuário.",
+                    required = true
+            )
+    }
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuário encontrado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     @GetMapping("{id}")
     public ResponseEntity<UserDto> findUserById(@PathVariable Long id){
         UserDto user = userService.findUserById(id);
