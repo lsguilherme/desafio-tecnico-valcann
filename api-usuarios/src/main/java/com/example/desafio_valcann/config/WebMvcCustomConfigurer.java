6d2db895123a6091692a93cf4aa4662a9c1776b8
Guilherme Lacerda <83317386+lsguilherme@users.noreply.github.com>
2025-09-07 13:54:06 -0300

    feat: add CORS configuration

--- api-usuarios/src/main/java/com/example/desafio_valcann/config/WebMvcCustomConfigurer.java
+++ api-usuarios/src/main/java/com/example/desafio_valcann/config/WebMvcCustomConfigurer.java
@@ -2,6 +2,7 @@ package com.example.desafio_valcann.config;
 
 import com.example.desafio_valcann.interceptors.ControllerInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,5 +11,14 @@ public class WebMvcCustomConfigurer implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new ControllerInterceptor());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://localhost:5000", "http://localhost:8080")
+                .allowedMethods("GET")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
